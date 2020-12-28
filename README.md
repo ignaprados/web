8cd988c411ca3e245592a653ca54047b4c827577
Ignacio Prados <ignacioprados.ip@gmail.com>
2020-12-28 19:43:22 -0300

    Publicación de mi página web v2

---
+++ README.md
@@ -0,0 +1,10 @@
+#  WEB IGNACIO PRADOS
+ Mi Página Web Ofical.
+<br><br>
+ Creador: Ignacio Prados.
+
+https://ignacioprados.github.io/web
+
+## Preview
+
+![banner](https://raw.githubusercontent.com/IgnacioPrados/web/gh-pages/IGNACIO%20PRADOS%20WEBSITE/assets/img/preview.JPG)
---
+++ assets/js/main.js
@@ -0,0 +1,132 @@
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
+
+/*===== INTRO =====*/
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1.7, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+
+
+
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
+
+/*Remove menu mobile*/
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{});
+sr.reveal('.button',{delay: 200});
+sr.reveal('.home__img',{delay: 400});
+sr.reveal('.home__social-icon',{ interval: 200});
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{});
+sr.reveal('.about__subtitle',{delay: 400});
+sr.reveal('.about__text',{delay: 400});
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{});
+sr.reveal('.skills__text',{});
+sr.reveal('.skills__data',{interval: 200});
+sr.reveal('.hab__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200});
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200});
+
+
+/*BACK TO TOP BUTTON*/
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+    backToTopButton.classList.remove("btnExit");
+    backToTopButton.classList.add("btnEntrance");
+    backToTopButton.style.display = "block";
+    }
+}
+  else { // Hide backToTopButton
+    if(backToTopButton.classList.contains("btnEntrance")) {
+    backToTopButton.classList.remove("btnEntrance");
+    backToTopButton.classList.add("btnExit");
+    setTimeout(function() {
+        backToTopButton.style.display = "none";
+    }, 250);
+    }
+}
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+function smoothScrollBackToTop() {
+const targetPosition = 0;
+const startPosition = window.pageYOffset;
+const distance = targetPosition - startPosition;
+const duration = 750;
+let start = null;
+
+window.requestAnimationFrame(step);
+
+function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+}
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
+
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
