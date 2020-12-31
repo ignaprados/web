fa895c83e093596fc1c9eb4d57e504d18b197f5d
Ignacio Prados <ignacioprados.ip@gmail.com>
2020-12-31 02:23:34 -0300

    FIX MOBILE BUG - COMMENTED AND CLEAN CODE

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
 
-/*===== INTRO =====*/
+
+/* ===== INTRO ===== */
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1.7, stagger: 0.25 });
@@ -11,9 +12,7 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 
 
-
-
-/*===== MENU SHOW =====*/
+/* ===== MENU SHOW ===== */
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -26,7 +25,8 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== ACTIVE AND REMOVE MENU =====*/
+
+/* ===== ACTIVE AND REMOVE MENU ===== */
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
@@ -40,7 +40,8 @@ navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+
+/* ===== SCROLL REVEAL ANIMATION ===== */
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -48,32 +49,32 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*SCROLL HOME*/
+
+/* = SCROLL HOME = */
 sr.reveal('.home__title',{});
 sr.reveal('.button',{delay: 200});
 sr.reveal('.home__img',{delay: 400});
 sr.reveal('.home__social-icon',{ interval: 200});
 
-/*SCROLL ABOUT*/
+/* = SCROLL ABOUT = */
 sr.reveal('.about__img',{});
 sr.reveal('.about__subtitle',{delay: 400});
 sr.reveal('.about__text',{delay: 400});
 
-/*SCROLL SKILLS*/
+/* = SCROLL SKILLS = */
 sr.reveal('.skills__subtitle',{});
 sr.reveal('.skills__text',{});
 sr.reveal('.skills__data',{interval: 200});
 sr.reveal('.hab__img',{delay: 600});
 
-/*SCROLL WORK*/
+/* = SCROLL WORK = */
 sr.reveal('.work__img',{interval: 200});
 
-/*SCROLL CONTACT*/
+/* = SCROLL CONTACT = */
 sr.reveal('.contact__input',{interval: 200});
 
 
-/*BACK TO TOP BUTTON*/
-
+/* ===== BACK TO TOP BUTTON ===== */
 const backToTopButton = document.querySelector("#back-to-top-btn");
 
 window.addEventListener("scroll", scrollFunction);
@@ -126,6 +127,7 @@ function easeInOutCubic(t, b, c, d) {
 	t -= 2;
 	return c/2*(t*t*t + 2) + b;
 };
+
 
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
