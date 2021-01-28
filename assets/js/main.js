4ed8dc6e14bcdf558fb8f4b0713f728b481078b3
Ignacio Prados <ignacioprados.ip@gmail.com>
2021-01-28 19:32:24 -0300

    Pop up In Develop

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,6 +209,24 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+
+/* ===== POPUP WORK=====*/
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+    popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
