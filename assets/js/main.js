a37688b4bf2e1969f15395abfdde845e003fc817
Ignacio Prados <ignacioprados.ip@gmail.com>
2021-01-29 15:26:52 -0300

    WORK POP UPS FINISHED

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,7 +210,7 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 
-/* ===== POPUP WORK=====*/
+/* ===== POPUP WORK =====*/
 var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
 	overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
@@ -227,6 +227,90 @@ btnCerrarPopup.addEventListener('click', function(e){
     popup.classList.remove('active');
 });
 
+/* ===== POPUP WORK 2 =====*/
+var btnAbrirPopup2 = document.getElementById('btn-abrir-popup2'),
+	overlay2 = document.getElementById('overlay2'),
+	popup2 = document.getElementById('popup2'),
+    btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2');
+
+btnAbrirPopup2.addEventListener('click', function(){
+	overlay2.classList.add('active');
+    popup2.classList.add('active');
+});
+
+btnCerrarPopup2.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay2.classList.remove('active');
+    popup2.classList.remove('active');
+});
+
+/* ===== POPUP WORK 3 =====*/
+var btnAbrirPopup3 = document.getElementById('btn-abrir-popup3'),
+	overlay3 = document.getElementById('overlay3'),
+	popup3 = document.getElementById('popup3'),
+    btnCerrarPopup3 = document.getElementById('btn-cerrar-popup3');
+
+btnAbrirPopup3.addEventListener('click', function(){
+	overlay3.classList.add('active');
+    popup3.classList.add('active');
+});
+
+btnCerrarPopup3.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay3.classList.remove('active');
+    popup3.classList.remove('active');
+});
+
+/* ===== POPUP WORK 4 =====*/
+var btnAbrirPopup4 = document.getElementById('btn-abrir-popup4'),
+	overlay4 = document.getElementById('overlay4'),
+	popup4 = document.getElementById('popup4'),
+    btnCerrarPopup4 = document.getElementById('btn-cerrar-popup4');
+
+btnAbrirPopup4.addEventListener('click', function(){
+	overlay4.classList.add('active');
+    popup4.classList.add('active');
+});
+
+btnCerrarPopup4.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay4.classList.remove('active');
+    popup4.classList.remove('active');
+});
+
+/* ===== POPUP WORK 5 =====*/
+var btnAbrirPopup5 = document.getElementById('btn-abrir-popup5'),
+	overlay5 = document.getElementById('overlay5'),
+	popup5 = document.getElementById('popup5'),
+    btnCerrarPopup5 = document.getElementById('btn-cerrar-popup5');
+
+btnAbrirPopup5.addEventListener('click', function(){
+	overlay5.classList.add('active');
+    popup5.classList.add('active');
+});
+
+btnCerrarPopup5.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay5.classList.remove('active');
+    popup5.classList.remove('active');
+});
+
+/* ===== POPUP WORK 6 =====*/
+var btnAbrirPopup6 = document.getElementById('btn-abrir-popup6'),
+	overlay6 = document.getElementById('overlay6'),
+	popup6 = document.getElementById('popup6'),
+    btnCerrarPopup6 = document.getElementById('btn-cerrar-popup6');
+
+btnAbrirPopup6.addEventListener('click', function(){
+	overlay6.classList.add('active');
+    popup6.classList.add('active');
+});
+
+btnCerrarPopup6.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay6.classList.remove('active');
+    popup6.classList.remove('active');
+});
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
