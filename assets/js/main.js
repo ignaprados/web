f3be220f44b6765cdc56c1dffea7253942256966
Ignacio Prados <ignacioprados.ip@gmail.com>
2021-06-02 13:22:13 -0300

    Experience Section Add

--- assets/js/main.js
+++ assets/js/main.js
@@ -358,6 +358,29 @@ btnCerrarPopup6.addEventListener('click', function(e){
 	overlay6.classList.remove('active');
     popup6.classList.remove('active');
 });
+
+/*========== QUALIFICATION  TABS ==========*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
 /* ===== IGNACIO PRADOS ===== */
