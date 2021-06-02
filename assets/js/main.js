95cbdd0840e98566e7aa7445bd7e3aebdb22ab63
Ignacio Prados <ignacioprados.ip@gmail.com>
2021-06-01 21:41:52 -0300

    Mouse Move Follow Basic

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,53 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 
 
+/* ===== SIGUERATONES ===== */
+
+var browserPrefix = "";
+var usrAg = navigator.userAgent;
+if (usrAg.indexOf("Chrome") > -1 || usrAg.indexOf("Safari") > -1) {
+  browserPrefix = "-webkit-";
+} else if (usrAg.indexOf("Opera") > -1) {
+  browserPrefix = "-o";
+} else if (usrAg.indexOf("Firefox") > -1) {
+  browserPrefix = "-moz-";
+} else if (usrAg.indexOf("MSIE") > -1) {
+  browserPrefix = "-ms-";
+}
+
+//TweenLite.defaultEase = Power3.easeInOut;
+
+var $cursor = jQuery("#js-cursor");
+var $cursor__big = jQuery("#js-cursor__big");
+var $cursor__small = jQuery("#js-cursor__small");
+var $links = jQuery(".curserhover, .call-to-action");
+
+
+jQuery(window).on("mousemove", function (e) {
+  $cursor.css(
+    browserPrefix + "transform",
+    "translate(" + e.pageX + "px, " + e.pageY + "px)",
+  );
+});
+
+$links.on("mouseenter", function (e) {
+  $cursor__big.css(browserPrefix + "transform", "scale(1)");
+});
+
+$links.on("mouseleave", function (e) {
+  $cursor__big.css(browserPrefix + "transform", "scale(0)");
+});
+
+$links.on("mouseenter", function (e) {
+  $cursor__small.css(browserPrefix + "transform", "scale(0)");
+});
+
+$links.on("mouseleave", function (e) {
+  $cursor__small.css(browserPrefix + "transform", "scale(1)");
+});
+
+
+
 /* ===== MENU SHOW ===== */
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
