d08d2c8cc2659e760c0c2d013a2eceab8ef9522b
Ignacio Prados <ignacioprados.ip@gmail.com>
2021-06-03 01:25:01 -0300

    skillset slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -385,6 +385,30 @@ tabs.forEach(tab =>{
     })
 })
 
+/* ========== SKILLS SLIDER ==========*/
+$(document).ready(function() {
+  $('.customer-logos').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }]
+  });
+});
+
 
 
 /* ===== IGNACIO PRADOS ===== */
