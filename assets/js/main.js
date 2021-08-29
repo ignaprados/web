be45f47f64f8785518af78b4c16523132949aebb
Ignacio Prados <ignacioprados.ip@gmail.com>
2021-08-29 19:42:07 -0300

    Update Intro Speed x2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,16 @@
 
 /* ===== INTRO ===== */
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
+/*
 tl.to(".text", { y: "0%", duration: 1.7, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });*/
 
+tl.to(".text", { y: "0%", duration: .85, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: .75, delay: 0.25 });
+tl.to(".intro", { y: "-100%", duration: .5 }, "-=.5");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: .40 });
 
 /* ===== SIGUERATONES ===== */
 
