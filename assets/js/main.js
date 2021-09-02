965ae02cffbf6f39e75a25ebc9f1c7f0016e4857
Ignacio Prados <ignacioprados.ip@gmail.com>
2021-09-02 10:15:32 -0300

    Fix Confetti

--- assets/js/main.js
+++ assets/js/main.js
@@ -338,10 +338,7 @@ var confetti = {
 		return e
 	};
 	var t = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame,
-		/*n = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"],*/
-		/*n = [ "rgba(113,	96,	137,", "rgba(83,	44,	137,", "rgba(144,	123,	174,", "rgba(142,	126,	177,", "rgba(0,	0,	0," ]*/
-    /*n = ["rgba(238,	197,	25,", "rgba(214,	177,	22," ]*/
-    n = ["rgba(206,	184,	242," ]
+		n = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"],
     e = !1,
 		i = !1,
 		o = Date.now(),
